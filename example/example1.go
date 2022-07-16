84e5710dbd3478f41511c081e6e23962235128fa
Kyle Tadashi <devgru0114@gmail.com>
2022-07-16 01:07:06 -0400

    update rules

--- example/example1.go
+++ example/example1.go
@@ -32,6 +32,7 @@ var directRouteEndPoint = "https://api.nodereal.io/direct-route"
 
 // var account1, _ = utils.FromHexKey("299d9a2ceda42af9ae8936fdafc680a764f324689a3d0f516ef3cad5048b692b")
 // var account2, _ = utils.FromHexKey("ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80")
+
 var account1, _ = utils.FromHexKey("6e22bd008464f3c6a045b711fdc696d5afab7e95594ef299013a930a1a916f68")
 var account2, _ = utils.FromHexKey("299d9a2ceda42af9ae8936fdafc680a764f324689a3d0f516ef3cad5048b692b")
 
@@ -73,49 +74,50 @@ func sendBNBByBundleDemo() {
 
 	// testnet chain id
 	//chainId := big.NewInt(97)
-	valueToTransfer := big.NewInt(200000 * params.GWei)
-	gasLimit := uint64(21000)
+	valueToTransfer := big.NewInt(100 * params.GWei)
+	gasLimit := uint64(23000)
 
-	for i := 0; i < 10; i++ {
-		tx1, _, _ := utils.SignTransaction(account1, account2.Addr, valueToTransfer, nil, n1 + uint64(i), gasLimit, price, chainId)
+	tx1, hash1, _ := utils.SignTransaction(account1, account2.Addr, valueToTransfer, nil, n1, gasLimit, price, chainId)
+	tx2, hash2, _ := utils.SignTransaction(account1, account2.Addr, valueToTransfer, nil, n1+1, gasLimit, price, chainId)
 
-		maxTime := uint64(time.Now().Unix() + 80)
+	maxTime := uint64(time.Now().Unix() + 80)
 
-		bundle := &client.SendBundleArgs{
-			Txs:               []string{hexutil.Encode(tx1)},
-			MaxBlockNumber:    "",
-			MinTimestamp:      nil,
-			MaxTimestamp:      &maxTime,
-			RevertingTxHashes: nil,
-		}
-		bundleHash, err := directClient.SendBundle(context.Background(), bundle)
-		
-		fmt.Printf("successfull send bundle, hash %v\n%v\n", bundleHash, err)
-
-		queryBundle, err := directClient.GetBundleByHash(context.Background(), bundleHash)
-		if err != nil || queryBundle == nil {
-			log.Fatalf("failed to query bundle %v", err)
-		}
-
-		bz, _ := json.Marshal(queryBundle)
-		fmt.Printf("The bundle is %s\n", string(bz))
+	bundle := &client.SendBundleArgs{
+		Txs:               []string{hexutil.Encode(tx1), hexutil.Encode(tx2)},
+		MaxBlockNumber:    "",
+		MinTimestamp:      nil,
+		MaxTimestamp:      &maxTime,
+		RevertingTxHashes: nil,
 	}
-	
+	bundleHash, err := directClient.SendBundle(context.Background(), bundle)
+	if err != nil {
+		log.Fatalf("failed to send bundle %v", err)
+	}
+	fmt.Printf("successfull send bundle, hash %v\n", bundleHash)
 
-	// found := false
-	// for i := 0; i < 42; i++ {
-	// 	r1, err1 := rpcClient.TransactionReceipt(context.Background(), hash1)
-	// 	if r1 != nil && err1 == nil {
-	// 		found = true
-	// 		break
-	// 	}
-	// 	time.Sleep(3 * time.Second)
-	// }
-	// if found {
-	// 	fmt.Println("bundle verified on chain")
-	// } else {
-	// 	log.Fatalf("bundle failed to be verified on chain or timeout")
-	// }
+	queryBundle, err := directClient.GetBundleByHash(context.Background(), bundleHash)
+	if err != nil || queryBundle == nil {
+		log.Fatalf("failed to query bundle %v", err)
+	}
+
+	bz, _ := json.Marshal(queryBundle)
+	fmt.Printf("The bundle is %s\n", string(bz))
+
+	found := false
+	for i := 0; i < 42; i++ {
+		r1, err1 := rpcClient.TransactionReceipt(context.Background(), hash1)
+		r2, err2 := rpcClient.TransactionReceipt(context.Background(), hash2)
+		if r1 != nil && err1 == nil && r2 != nil && err2 == nil {
+			found = true
+			break
+		}
+		time.Sleep(3 * time.Second)
+	}
+	if found {
+		fmt.Println("bundle verified on chain")
+	} else {
+		log.Fatalf("bundle failed to be verified on chain or timeout")
+	}
 }
 
 /**
@@ -194,63 +196,63 @@ func sendBNBAndTokenBundleDemo() {
 		log.Fatalf("failed to get bundle price%v", err)
 	}
 
-	n1, _ := rpcClient.PendingNonceAt(context.Background(), account1.Addr)
 	n2, _ := rpcClient.PendingNonceAt(context.Background(), account2.Addr)
-	fmt.Printf("successfull send bundle, hash %v\n", n2);
 
 	chainId := big.NewInt(56)
 
 	// testnet chain id
 	//chainId := big.NewInt(97)
 	valueToTransfer := big.NewInt(0 * params.GWei)
-	gasLimit := uint64(0)
+	gasLimit := uint64(500000)
 
 	bep20ABI, _ := abi.JSON(strings.NewReader(eabi.BEP20ABI))
-	data2, _ := bep20ABI.Pack("transfer", account1.Addr, big.NewInt(7350000000000000))
+	// data2, _ := bep20ABI.Pack("transfer", account1.Addr, big.NewInt(7350000000000000))
+	data2, _ := bep20ABI.Pack("transfer", account1.Addr, big.NewInt(1))
 
-	tx1, hash1, _ := utils.SignTransaction(account1, account2.Addr, valueToTransfer, nil, n1, gasLimit, price, chainId)
-	tx2, hash2, _ := utils.SignTransaction(account2, common.HexToAddress("0x53dcD4eF8E21FE014594a0854c4271a0623B31eC"), valueToTransfer, data2, n2, gasLimit, price, chainId)
+	for i := 0; i < 1000; i++ {
+		tx2, _, _ := utils.SignTransaction(account2, common.HexToAddress("0xe9e7cea3dedca5984780bafc599bd69add087d56"), valueToTransfer, data2, n2 + uint64(i), gasLimit, price, chainId)
 
-	maxTime := uint64(time.Now().Unix() + 80)
-
-	bundle := &client.SendBundleArgs{
-		Txs:               []string{ hexutil.Encode(tx1), hexutil.Encode(tx2)},
-		MaxBlockNumber:    "",
-		MinTimestamp:      nil,
-		MaxTimestamp:      &maxTime,
-		RevertingTxHashes: nil,
-	}
-	bundleHash, err := directClient.SendBundle(context.Background(), bundle)
-	if err != nil {
-		log.Fatalf("failed to send bundle %v", err)
-	}
-	fmt.Printf("successfull send bundle, hash %v\n", bundleHash)
-
-	queryBundle, err := directClient.GetBundleByHash(context.Background(), bundleHash)
-	if err != nil || queryBundle == nil {
-		log.Fatalf("failed to query bundle %v", err)
-	}
-
-	bz, _ := json.Marshal(queryBundle)
-	fmt.Printf("The bundle is %s\n", string(bz))
-
-	found := false
-	for i := 0; i < 42; i++ {
-		r1, err1 := rpcClient.TransactionReceipt(context.Background(), hash1)
-		r2, err2 := rpcClient.TransactionReceipt(context.Background(), hash2)
-		if r1 != nil && err1 == nil && r2 != nil && err2 == nil {
-			found = true
-			break
+		maxTime := uint64(time.Now().Unix() + 80)
+	
+		bundle := &client.SendBundleArgs{
+			Txs:               []string{ hexutil.Encode(tx2)},
+			MaxBlockNumber:    "",
+			MinTimestamp:      nil,
+			MaxTimestamp:      &maxTime,
+			RevertingTxHashes: nil,
 		}
-		time.Sleep(3 * time.Second)
+		bundleHash, err := directClient.SendBundle(context.Background(), bundle)
+		if err != nil {
+			log.Fatalf("failed to send bundle %v", err)
+		}
+		fmt.Printf("successfull send bundle, hash %v\n", bundleHash)
+
+		queryBundle, err := directClient.GetBundleByHash(context.Background(), bundleHash)
+		if err != nil || queryBundle == nil {
+			log.Fatalf("failed to query bundle %v", err)
+		}
+
+		bz, _ := json.Marshal(queryBundle)
+		fmt.Printf("The bundle is %s\n", string(bz))
+
 	}
-	if found {
-		fmt.Println("bundle verified on chain")
-	} else {
-		log.Fatalf("bundle failed to be verified on chain or timeout")
-	}
+	// found := false
+	// for i := 0; i < 42; i++ {
+	// 	r1, err1 := rpcClient.TransactionReceipt(context.Background(), hash1)
+	// 	r2, err2 := rpcClient.TransactionReceipt(context.Background(), hash2)
+	// 	if r1 != nil && err1 == nil && r2 != nil && err2 == nil {
+	// 		found = true
+	// 		break
+	// 	}
+	// 	time.Sleep(3 * time.Second)
+	// }
+	// if found {
+	// 	fmt.Println("bundle verified on chain")
+	// } else {
+	// 	log.Fatalf("bundle failed to be verified on chain or timeout")
+	// }
 }
 
 func main() {
-	sendBNBByBundleDemo()
+	sendBNBAndTokenBundleDemo()
 }
